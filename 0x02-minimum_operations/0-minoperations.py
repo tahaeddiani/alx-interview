85c5cc11a99490b95a0348bb47e9ed3a137cb1f1
tahaeddiani <tahaeddiani2@gmail.com>
2024-01-20 18:55:43 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,8 +7,8 @@
 
 def minOperations(n):
     """
-        A function that calculates the fewest number of operations 
-    """
+        A function that calculates the fewest number of operations
+        """
 
     now = 1
     start = 0
